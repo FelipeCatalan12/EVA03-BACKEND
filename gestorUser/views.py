ed36deb4d293d3446725c5bc109be73d02b73463
MartinLopezDev <98781432+martinskerr@users.noreply.github.com>
2023-12-05 19:26:55 -0300

    landing page add

--- gestorUser/views.py
+++ gestorUser/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    data = {
+        'title': 'Clinica Veterinaria Chucky',
+        'message': 'Bienvenido a la clinica veterinaria Chucky',
+        'info':'Esta es una clinica veterinaria que se encarga de cuidar a tus mascotas, para que puedas estar tranquilo y seguro de que tu mascota esta en buenas manos.',
+        'informacion2': 'Podras ver e ingresar a los diferentes servicios que ofrecemos, como por ejemplo: Ingresar a la tienda, ver los servicios que ofrecemos, ver los productos que ofrecemos, ver los medicamentos que ofrecemos, ver los clientes que tenemos, ver los empleados que tenemos,'
+    }
+    return render(request, 'home.html', data)
--- inventarioVeterinariaFelipeCyMartinL/urls.py
+++ inventarioVeterinariaFelipeCyMartinL/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
-from templates import *
+from gestorUser.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cuentas/', include("django.contrib.auth.urls")),
-    path('', TemplateView.as_view(template_name="home.html"), name='home')
+    path('',index)
 ]
+
+
