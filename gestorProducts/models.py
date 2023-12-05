c9edbe7873cdeb81a48d8b9164a460fe11581ddf
FelipeCatalan12 <122119968+FelipeCatalan12@users.noreply.github.com>
2023-12-05 19:23:44 -0300

    Add files via upload

---
+++ gestorProducts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model):
+    nombre_categoria = models.CharField(max_length=55)
+
+    def __str__(self):
+        return self.nombre_categoria    
+
+class Productos(models.Model):
+    nombre_producto = models.CharField(max_length=55)
+    descripcion_producto = models.CharField(max_length=55)
+    precio = models.IntegerField(max_length=20)
+    categoria_producto = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre_producto   
