5706dd5db594bd3b51ca6a29999f9ad5235d4b25
MartinLopezDev <98781432+martinskerr@users.noreply.github.com>
2023-12-05 22:43:06 -0300

    add url to panel

--- gestorProducts/views.py
+++ gestorProducts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def panel(request):
+    data = {
+        'title': 'Panel de Administración',
+    }
+    return render(request, 'panel/index.html')
--- inventarioVeterinariaFelipeCyMartinL/urls.py
+++ inventarioVeterinariaFelipeCyMartinL/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from gestorUser.views import index, index_master
+from gestorUser.views import index
+from gestorProducts.views import panel
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('panel/',panel)
+
 ]
 
 
